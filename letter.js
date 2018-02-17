f24a3d2491fbe1f47594dcd1a5bdef4378b4f159
jelizabethscott <jescottphoto@gmail.com>
2018-02-17 16:18:51 -0500

    update

--- letter.js
+++ letter.js
@@ -1,18 +1,25 @@
 function letter(value) {  
 	this.value = value;
-	this.show = false;
-	if(this.value === ''){
-		this.show = true;
-	}
+	this.show = !/[a-z1-9]/i.test(value);
 }
-
-letter.prototype.printLetter = function {
-	if(this.show){
+	
+letter.prototype.construct = function() {
+	if(this.show === true){
 		return this.value;
 	}
 		return"*";
+};
 
-}
+letter.prototype.results = function() {
+	return this.value;
+};
 
+letter.prototype.printLetter = function (valuePrinted) {
+	if (valuePrinted.toUpperCase() === this.value.toUpperCase()) {
+		this.show = true;
+		return true;
+	}
+	return false;
+};
 
 module.exports = letter;
